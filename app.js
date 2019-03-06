247d3e7ae638975ba2bb65f2edc5c2d79890654d
Maxim Tereshchenko <maxter@Maxims-MacBook-Air.local>
2019-03-06 23:33:02 +0200

    The query for users table has been added with a dummy row

--- app.js
+++ app.js
@@ -32,12 +32,12 @@ const { Client } = require('pg');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: true,//TODO uncomment before pushing
 });
 
 client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+client.query('SELECT * FROM users;', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
