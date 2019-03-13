94155d60786c2150495790aa7921e0a82bc0036e
Maxim Tereshchenko <maxter@Maxims-MacBook-Air.local>
2019-03-13 21:12:21 +0200

    the new of corresponding between extension and bot is starting to be impletemnted

--- app.js
+++ app.js
@@ -23,21 +23,13 @@ const
 if (process.env.NODE_ENV === 'stage') {
 	require('dotenv').load();
 } else {
-	const pup = require('./puppeteerScript');
-	console.log(pup);
-	pup.puppeteerGetJSONfromPage().catch(err => console.log(err));
+	// const pup = require('./puppeteerScript');
+	// console.log(pup);
+	// pup.puppeteerGetJSONfromPage(url).catch(err => console.log(err));
 	console.log(process.env.TEST);
 }
 
 console.log(process.env.NODE_ENV);
-// const psidToFbid = new PsidToFbid("630204857440599",{page_token: "EAADqMIVDZBpkBAFKMWnBdN7FjhbGoLj6Ibomo9BudBdPWMEs2tchRvC805z54bDVy4GlUvoTglxaxGMqNnYluqqfpgDLCHVURFH8l4N8HU7mkZAwDTqD7TzS3oqH56MrAhKFZAd7hvihz8y95uGzKE4oAmwWQHy3CF5nBPr9Uvx9b67c7Umt3VE5ivZB5aC31AKJmBZAeZCAZDZD"}); //TODO make dynamic
-
-// psidToFbid.fetchPageToken("EAADqMIVDZBpkBAEvRMR9qVfW9Or2j1TBbe1u1bZACepPxjwStHokSQi9B6cdRqsSnkqZABXCEUcPoulMEH76dzEIF4VlujZAOem22D2Rzd8Qg1wyxko1Ch4WCTshYSZC0YmssD2SfUxHaTlGbkZBaG2nad1QxmWnAr11xKNcrgAiiIpZC2ZAx9WPFRccwwre7ZAUKcWghUBP42XHdg9STnFjA1CL2rmupUPSzBZBPZCSlHWZCwZDZD")
-//     .then((page_token) => {
-//       console.log("Setup complete", page_token);
-//     }).catch(() => {
-//   console.log("Setup failed");
-// }); //TODO uncomment before pushing
 
 const {Client} = require('pg');
 
@@ -60,6 +52,15 @@ app.use(express.static('public'));
  * set them using environment variables or modifying the config file in /config.
  *
  */
+
+const psidToFbid = new PsidToFbid("630204857440599", {page_token: "EAADqMIVDZBpkBAFKMWnBdN7FjhbGoLj6Ibomo9BudBdPWMEs2tchRvC805z54bDVy4GlUvoTglxaxGMqNnYluqqfpgDLCHVURFH8l4N8HU7mkZAwDTqD7TzS3oqH56MrAhKFZAd7hvihz8y95uGzKE4oAmwWQHy3CF5nBPr9Uvx9b67c7Umt3VE5ivZB5aC31AKJmBZAeZCAZDZD"}); //TODO make dynamic
+
+// psidToFbid.fetchPageToken("EAADqMIVDZBpkBAEvRMR9qVfW9Or2j1TBbe1u1bZACepPxjwStHokSQi9B6cdRqsSnkqZABXCEUcPoulMEH76dzEIF4VlujZAOem22D2Rzd8Qg1wyxko1Ch4WCTshYSZC0YmssD2SfUxHaTlGbkZBaG2nad1QxmWnAr11xKNcrgAiiIpZC2ZAx9WPFRccwwre7ZAUKcWghUBP42XHdg9STnFjA1CL2rmupUPSzBZBPZCSlHWZCwZDZD")
+//     .then((page_token) => {
+//       console.log("Setup complete", page_token);
+//     }).catch(() => {
+//   console.log("Setup failed");
+// }); //TODO uncomment before pushing
 
 // App Secret can be retrieved from the App Dashboard
 const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
@@ -303,29 +304,35 @@ function receivedMessage(event) {
 	var message = event.message;
 
 	//psid plugin
-	psidToFbid.getFromWebhookEvent(event).then(fbid => {
-		console.log("Got psid = " + senderID + ", fbid = " + fbid);
-
-		//insert the user to Postgres DB
-		const text = 'INSERT INTO users(psid, userid)\n' +
-			'VALUES($1, $2)\n' +
-			'ON CONFLICT (psid) \n' +
-			'DO\n' +
-			'UPDATE\n' +
-			'SET userid = EXCLUDED.userid;\n';
-		const values = [senderID, fbid];
-
-		client.query(text, values)
-			.then(res => {
-				console.log(res.rows[0]);
-			})
-			.catch(e => console.error(e.stack));
-	});
-
+	// psidToFbid.getFromWebhookEvent(event).then(fbid => {
+	// 	console.log("Got psid = " + senderID + ", fbid = " + fbid);
+	//
+	// 	//insert the user to Postgres DB
+	// 	const text = 'INSERT INTO users(psid, userid)\n' +
+	// 		'VALUES($1, $2)\n' +
+	// 		'ON CONFLICT (psid) \n' +
+	// 		'DO\n' +
+	// 		'UPDATE\n' +
+	// 		'SET userid = EXCLUDED.userid;\n';
+	// 	const values = [senderID, fbid];
+	//
+	// 	client.query(text, values)
+	// 		.then(res => {
+	// 			console.log(res.rows[0]);
+	// 		})
+	// 		.catch(e => console.error(e.stack));
+	// });
 
 	console.log("Received message for user %d and page %d at %d with message:",
 		senderID, recipientID, timeOfMessage);
 	console.log(JSON.stringify(message));
+
+	request.get(`https://graph.facebook.com/${senderID}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`, (err, res) => {
+		if (err) {
+			return console.log(err);
+		}
+		console.log(JSON.stringify(res.body));
+	});
 
 	var isEcho = message.is_echo;
 	var messageId = message.mid;
--- puppeteerScript.js
+++ puppeteerScript.js
@@ -1,10 +1,10 @@
 const puppeteer = require('puppeteer');
 
 /* jshint ignore:start */
-exports.puppeteerGetJSONfromPage = async () => {
+exports.puppeteerGetJSONfromPage = async (url) => {
 	const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
 	const page = await browser.newPage();
-	await page.goto('https://api.facebook.com/restserver.php?api_key=3e7c78e35a76a9299309885393b02d97&email=tereshchenkomax%40gmail.com&format=JSON&generate_session_cookies=1&locale=en_us&method=auth.login&password=21mnegod1488&return_ssl_resources=0&v=1.0&sig=ec16c363b6e8215acf144f617ed15985');
+	await page.goto(url);
 
 	let content = await page.content();
 
